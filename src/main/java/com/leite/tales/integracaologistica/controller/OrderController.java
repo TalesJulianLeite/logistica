323719c89749ed37a76e01e6926c8629a06f2282
Tales Julian Leite <talesjulianl@gmail.com>
2024-04-07 23:58:36 -0300

    Ajuste no nome da controller

--- src/main/java/com/leite/tales/integracaologistica/controller/OrderController.java
+++ src/main/java/com/leite/tales/integracaologistica/controller/OrderController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/order")
 @CrossOrigin(origins = "*")
-public class OrdearController {
+public class OrderController {
 
   @Autowired
   private OrderService service;
